b79a4099ab8b252f3ae887fe5e728023ce98172f
JuanbyGuada <juanby2009@yahoo.com.ar>
2024-04-26 11:14:17 -0300

    vistas, url y migracion de med

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,11 @@ urlpatterns = [
     path("mascotas/nuevo/", view=views.pets_form, name="pets_form"),
     path("mascotas/editar/<int:id>/", view=views.pets_form, name="pets_edit"),
     path("mascotas/eliminar/", view=views.pets_delete, name="pets_delete"),
+    #MEDICINES
+    path("medicamentos/", view=views.medicines_repository, name="medicines_repo"),
+    path("medicamentos/nuevo/", view=views.medicines_form, name="medicines_form"),
+    path("medicamentos/editar/<int:id>/", view=views.medicines_form, name="medicines_edit"),
+    path("medicamentos/eliminar/", view=views.medicines_delete, name="medicines_delete"),
 ]
 
 
