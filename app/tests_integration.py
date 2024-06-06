cd9a5477529216068568eba357009ca5fc9f6c74
Aylen <aylentastaca@gmail.com>
2024-06-06 00:23:32 -0300

    Guardando cambios locales antes de merge

--- app/tests_integration.py
+++ app/tests_integration.py
@@ -27,7 +27,7 @@ class ClientsTest(TestCase):
             reverse("clients_form"),
             data={
                 "name": "Juan Sebastian Veron",
-                "phone": "221555232",
+                "phone": "54221555232",
                 "address": "13 y 44",
                 "email": "brujita75@hotmail.com",
             },
@@ -36,7 +36,7 @@ class ClientsTest(TestCase):
         self.assertEqual(len(clients), 1)
 
         self.assertEqual(clients[0].name, "Juan Sebastian Veron")
-        self.assertEqual(clients[0].phone, "221555232")
+        self.assertEqual(clients[0].phone, "54221555232")
         self.assertEqual(clients[0].address, "13 y 44")
         self.assertEqual(clients[0].email, "brujita75@hotmail.com")
 
@@ -61,19 +61,20 @@ class ClientsTest(TestCase):
             reverse("clients_form"),
             data={
                 "name": "Juan Sebastian Veron",
-                "phone": "221555232",
+                "phone": "54221555232",
                 "address": "13 y 44",
                 "email": "brujita75",
             },
         )
 
         self.assertContains(response, "Por favor ingrese un email valido")
+        
 
     def test_edit_user_with_valid_data(self):
         client = Client.objects.create(
             name="Juan Sebastián Veron",
             address="13 y 44",
-            phone="221555232",
+            phone="54221555232",
             email="brujita75@hotmail.com",
         )
 
@@ -82,8 +83,12 @@ class ClientsTest(TestCase):
             data={
                 "id": client.id,
                 "name": "Guido Carrillo",
+                "phone": client.phone,
+                "address": client.address,
+                "email": client.email,
             },
         )
+
 
         # redirect after post
         self.assertEqual(response.status_code, 302)
