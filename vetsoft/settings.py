b69d77c8fd3f145bffa6724e182e6c45785dd9c0
Aylen <aylentastaca@gmail.com>
2024-06-06 15:21:14 -0300

    se arregla conflictos

--- vetsoft/settings.py
+++ vetsoft/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-#import os
+import os
 
 
 
@@ -24,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv("DEBUG", "False").upper() == "TRUE"
+DEBUG = os.getenv("DEBUG", "False").upper() == "TRUE"
 
-DEBUG = True
-SECRET_KEY= "django-insecure-p)^5i@33!)v)l7*c#q)%j(g5d+**-yo%)6l*vg!gs_w-e=^_ig"
+#DEBUG = True
+#SECRET_KEY= "django-insecure-p)^5i@33!)v)l7*c#q)%j(g5d+**-yo%)6l*vg!gs_w-e=^_ig"
 
 
 
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = []  
 
